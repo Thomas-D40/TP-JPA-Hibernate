278da86fa7e3cc3fdd343162531b0176782600ea
Thomas-D40 <alexander.darracq@gmail.com>
2022-06-13 13:56:11 +0200

    Config Hibernate

--- src/main/java/fr/m2i/crm/Main.java
+++ src/main/java/fr/m2i/crm/Main.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class Main {
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm"); // Point d'entrée pour créer des requêtes
 		EntityManager entityManager = emf.createEntityManager();
 	
 	}
