16056a4a247d822288243ad315b49285395f7e9e
Thomas-D40 <alexander.darracq@gmail.com>
2022-06-13 15:05:27 +0200

    Optimisation du code

--- src/main/java/fr/m2i/crm/helper/SessionHelper.java
+++ src/main/java/fr/m2i/crm/helper/SessionHelper.java
@@ -6,12 +6,16 @@ import javax.persistence.Persistence;
 
 public class SessionHelper {
 	private static EntityManager entityManager;
-	private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm");
+	
+	
+	// A éviter car bouffe de la mémoire pour stocker cette info qui ne sert qu'à une reprise
+	// private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm");
 	
 	private SessionHelper() {};
 	
 	public static EntityManager getEntityManager() {
 		if (entityManager == null) {
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm");
 			entityManager = emf.createEntityManager();
 		}
 		return entityManager;
